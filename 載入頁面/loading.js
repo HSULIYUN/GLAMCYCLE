f63bf0f24e0aa0b581a2db142e87bdd08fd17302
hiuu <s1311434016@nutc.edu.tw>
2026-02-09 01:42:07 +0800

    Delete 載入頁面 directory

--- 載入頁面/loading.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-    var loadingScreen = document.getElementById('loading');
-    var content = document.querySelector('.content');
-    setTimeout(function () {
-        loadingScreen.style.display = 'none';
-        content.style.display = 'block';
-    }, 3000); // 延遲 3 秒後隱藏載入畫面並顯示主內容
-});

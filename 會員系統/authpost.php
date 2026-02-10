c55efcfdda12c47f1de31c4c56b2f69d258f93e2
hiuu <s1311434016@nutc.edu.tw>
2026-02-10 19:49:12 +0800

    會員-檢查使用者是否登入

--- 會員系統/authpost.php
+++ 會員系統/authpost.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
 
-// 檢查是否已登入
 if (!isset($_SESSION['username'])) {
-    // 使用 JavaScript 彈出提示框並重定向到登入頁面
     echo "<script>alert('使用討論串，需要先登入系統'); window.location.href='/小專檔案/會員系統/sign.php';</script>";
-    exit; // 終止後續代碼執行
+    exit; 
 }
 
 function getUserDisplayName() {

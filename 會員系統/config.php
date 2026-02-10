2160316c4642ba57271ba705c075724b812c2556
hiuu <s1311434016@nutc.edu.tw>
2026-02-10 19:50:59 +0800

    會員-資料庫連接

--- 會員系統/config.php
+++ 會員系統/config.php
@@ -1,16 +1,16 @@
 
 <?php
-/* 連接到會員資料庫 */
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "cycle";
 $port = 3308;
 
-// 建立連線
+
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// 檢查連線
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }

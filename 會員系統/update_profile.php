935e94f97cc1b4961c940f313fac458de7563aeb
hiuu <s1311434016@nutc.edu.tw>
2026-02-10 19:58:15 +0800

    會員-優化會員資料session紀錄

--- 會員系統/update_profile.php
+++ 會員系統/update_profile.php
@@ -1,7 +1,7 @@
 <?php
-/* 更新會員資料傳到資料庫 */
+
 session_start();
-include 'config.php';  // 引入连接配置
+include 'config.php'; 
 
 if (isset($_POST['username'], $_POST['email'], $_POST['phone'], $_POST['user_birthday'], $_POST['user_gender'])) {
     $userId = $_SESSION['user_id'];
@@ -11,7 +11,7 @@ if (isset($_POST['username'], $_POST['email'], $_POST['phone'], $_POST['user_bir
     $user_birthday = $conn->real_escape_string($_POST['user_birthday']);
     $user_gender = $conn->real_escape_string($_POST['user_gender']);
 
-    // 构造SQL语句
+
     $sql = "UPDATE users SET 
             username='$username', 
             email='$email', 
@@ -20,23 +20,23 @@ if (isset($_POST['username'], $_POST['email'], $_POST['phone'], $_POST['user_bir
             user_gender='$user_gender' 
             WHERE id='$userId'";
 
-    // 执行SQL语句
+
     if ($conn->query($sql) === TRUE) {
-        // 更新会话变量
+ 
         $_SESSION['username'] = $username;
         $_SESSION['email'] = $email;
         $_SESSION['phone'] = $phone;
         $_SESSION['user_birthday'] = $user_birthday;
         $_SESSION['user_gender'] = $user_gender;
 
-        // 弹窗提示并重定向
+       
         echo "<script>alert('資料成功更新!'); window.location.href='edit_profile.php';</script>";
     } else {
-        // 如果SQL执行失败，显示错误信息
+        
         echo "Error updating record: " . $conn->error;
     }
 } else {
-    // 如果必填字段有遗漏，提示错误
+   
     echo "All fields are required!";
 }
 $conn->close();

a59b62865d3fa58b6dbbb2abfe771166e23c95bb
hiuu <s1311434016@nutc.edu.tw>
2026-02-10 19:36:42 +0800

    資料庫連接

--- 首頁網站/db.php
+++ 首頁網站/db.php
@@ -1,14 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "root";  // 你的資料庫用戶名
-$password = "";  // 你的資料庫密碼
+$username = "root";  
+$password = "";  
 $dbname = "cycle";
-$port = 3308;  // 資料庫連接端口
+$port = 3308;  
 
-// 建立連接
+
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// 檢查連接
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }

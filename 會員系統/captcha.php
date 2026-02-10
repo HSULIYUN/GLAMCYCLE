ca64dd7c6f969fa54ac9db9fc78ae17c3dde5e64
hiuu <s1311434016@nutc.edu.tw>
2026-02-10 19:50:18 +0800

    會員-加上reCAPTCHA檢查機器人

--- 會員系統/captcha.php
+++ 會員系統/captcha.php
@@ -1,11 +1,11 @@
 <?php
-// Start output buffering
+
 session_start();
 
 function createCaptcha() {
     $captcha_code = '';
     for ($i = 0; $i < 6; $i++) {
-        $captcha_code .= chr(rand(65, 90)); // Generate A-Z
+        $captcha_code .= chr(rand(65, 90)); 
     }
     $_SESSION['captcha'] = $captcha_code;
     return $captcha_code;
@@ -18,10 +18,10 @@ function generateCaptchaImage($captcha_code) {
     $background = imagecolorallocate($image, 255, 255, 255);
     $text_color = imagecolorallocate($image, 0, 0, 0);
 
-    // Add captcha code to image
+
     imagestring($image, 5, 10, 10, $captcha_code, $text_color);
 
-    // Send image header and output the image
+ 
     header('Content-type: image/png');
     imagepng($image);
     imagedestroy($image);
